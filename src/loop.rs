c8be2cefed52c06316fb540d0e080d843035fb17
2814109 <2814109k@gmail.com>
2022-11-05 19:12:12 +0900

    first commit

---
+++ src/loop.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let v = &["apples", "cake", "coffee"];
+
+    for text in v {
+        println!("I like {}.", text);
+    }
+
+    let mut sum = 0;
+    for n in 1..11 {
+        sum += n;
+    }
+
+     println!("sum = {}", sum);
+    // assert_eq!(sum, 55);
+}
